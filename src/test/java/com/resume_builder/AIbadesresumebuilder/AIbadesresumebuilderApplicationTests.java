3e4143462c03c7316a49853d36d880d7a551e9d9
Diksha Hembade <dikashahembade@gmail.com>
2026-06-30 23:41:33 +0530

    Add source code

---
+++ src/test/java/com/resume_builder/AIbadesresumebuilder/AIbadesresumebuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resume_builder.AIbadesresumebuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AIbadesresumebuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
